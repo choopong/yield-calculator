b40f76fd843c67c03dd85931bde459be659aabde
Choopong Choosamer <cchoopong@gmail.com>
2026-08-23 00:56:28 +0700

    titleTemplate

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,7 @@ export default defineNuxtConfig({
   css: ['~/assets/css/main.css'],
   app: {
     head: {
-      title: 'Yield Calculator'
+      titleTemplate: 'Yield Calculator'
     }
   },
   features: {
